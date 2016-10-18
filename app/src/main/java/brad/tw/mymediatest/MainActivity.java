752eacf2b419ce8d6b1b02a66c22b053b26e5bae
Brad <brad@brad.tw>
2016-10-18 19:41:23 +0800

    v5

--- app/src/main/java/brad/tw/mymediatest/MainActivity.java
+++ app/src/main/java/brad/tw/mymediatest/MainActivity.java
@@ -30,12 +30,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_EXTERNAL_STORAGE)
+                Manifest.permission.RECORD_AUDIO)
                 != PackageManager.PERMISSION_GRANTED) {
 
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.RECORD_AUDIO},
                         123);
 
         }
@@ -65,11 +66,11 @@ public class MainActivity extends AppCompatActivity {
     public void test3(){
         isRecording = !isRecording;
         if (isRecording){
-            startRecording();
             recorder.setText("STOP");
+            startRecording();
         }else{
-            stopRecording();
             recorder.setText("START");
+            stopRecording();
         }
     }
 
