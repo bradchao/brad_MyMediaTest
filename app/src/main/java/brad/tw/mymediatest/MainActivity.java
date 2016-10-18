bdec745a3a3b2f7b7967fd1786400f91899dc8a2
Brad <brad@brad.tw>
2016-10-18 19:01:22 +0800

    v3

--- app/src/main/java/brad/tw/mymediatest/MainActivity.java
+++ app/src/main/java/brad/tw/mymediatest/MainActivity.java
@@ -4,6 +4,7 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private SoundPool sp;
@@ -17,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
         sp = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
         sound1 = sp.load(this, R.raw.cars004, 1);
         sound2 = sp.load(this, R.raw.cars017, 1);
-
     }
+
+    public void test1(View v){
+        sp.play(sound1,0.5f,0.5f,1,0,1);
+    }
+    public void test2(View v){
+        sp.play(sound2,0.5f,0.5f,1,0,1);
+    }
+
 }
