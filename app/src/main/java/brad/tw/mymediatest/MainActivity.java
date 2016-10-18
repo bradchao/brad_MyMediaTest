692224fa22757e8223118b5775c54c9b4c991a73
Brad <brad@brad.tw>
2016-10-18 18:55:19 +0800

    v2

--- app/src/main/java/brad/tw/mymediatest/MainActivity.java
+++ app/src/main/java/brad/tw/mymediatest/MainActivity.java
@@ -1,13 +1,22 @@
 package brad.tw.mymediatest;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private SoundPool sp;
+    private int sound1,sound2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sp = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
+        sound1 = sp.load(this, R.raw.cars004, 1);
+        sound2 = sp.load(this, R.raw.cars017, 1);
+
     }
 }
