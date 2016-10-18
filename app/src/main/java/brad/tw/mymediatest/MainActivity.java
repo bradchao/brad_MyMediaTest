dc73ea4fed0a9ecb820d71e9f6a5172ec0a1de80
Brad <brad@brad.tw>
2016-10-18 19:31:41 +0800

    v4

--- app/src/main/java/brad/tw/mymediatest/MainActivity.java
+++ app/src/main/java/brad/tw/mymediatest/MainActivity.java
@@ -1,19 +1,53 @@
 package brad.tw.mymediatest;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
+import android.media.MediaRecorder;
 import android.media.SoundPool;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     private SoundPool sp;
     private int sound1,sound2;
+    private boolean isRecording;
+    private MediaRecorder mRecorder;
+    private File sdroot;
+    private Button recorder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        123);
+
+        }
+
+        sdroot = Environment.getExternalStorageDirectory();
+        recorder = (Button)findViewById(R.id.recorder);
+        recorder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                test3();
+            }
+        });
 
         sp = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
         sound1 = sp.load(this, R.raw.cars004, 1);
@@ -26,5 +60,41 @@ public class MainActivity extends AppCompatActivity {
     public void test2(View v){
         sp.play(sound2,0.5f,0.5f,1,0,1);
     }
+
+
+    public void test3(){
+        isRecording = !isRecording;
+        if (isRecording){
+            startRecording();
+            recorder.setText("STOP");
+        }else{
+            stopRecording();
+            recorder.setText("START");
+        }
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(sdroot.getAbsolutePath() + "/brad.3gp");
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+            mRecorder.start();
+        } catch (Exception e) {
+            Log.e("brad", "prepare() failed");
+        }
+
+    }
+
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
 
 }
